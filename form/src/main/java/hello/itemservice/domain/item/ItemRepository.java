8a137d718152e999d5feadcb5727e1b768504313
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2023-03-16 22:29:53 +0900

    single check box codes
    single check box codes

--- form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -32,6 +32,10 @@ public class ItemRepository {
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
+        findItem.setOpen(updateParam.getOpen());
+        findItem.setItemType(updateParam.getItemType());
+        findItem.setRegions(updateParam.getRegions());
+        findItem.setDelveryCode(updateParam.getDelveryCode());
     }
 
     public void clearStore() {
